36179f22ac20616d119a4d2d0c2000fd054a18ff
Amy Jacobs <amelia.b.jacobs@gmail.com>
2017-10-29 13:57:10 -0700

    create javascript for full quiz & add additional question

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
     var atmosInput = $("input:radio[name=atmos]:checked").val();
     var tempInput = $("input:radio[name=temp]:checked").val();
     var colorInput = $("input:radio[name=color]:checked").val();
+    var foodInput = $("input:radio[name=food]:checked").val();
+
 
     if (atmosInput==="water") {
       faln +=1;
@@ -16,9 +18,46 @@ $(document).ready(function() {
       nebula +=1;
     }
 
+    if (tempInput==="cold" || tempInput=="hot") {
+      nebula +=1;
+    } else if (tempInput==="human") {
+      faln +=1;
+      xerus +=1;
+    }
+
+    if (colorInput==="blue") {
+      faln +=1;
+    } else if (colorInput ==="red") {
+      xerus +=1;
+    } else if (colorInput ==="green" || colorInput ==="infared") {
+      nebula +=1;
+    }
+
+    if (faln === xerus || faln === nebula || nebula === faln) {
+      if (foodInput === "pasta") {
+        xerus +=1;
+      } else if (foodInput === "nori") {
+        faln +=1;
+      } else if (foodInput === "solar") {
+        nebula +=1;
+      }
+    }
+
     alert("Faln:" + faln);
     alert("Xerus:" + xerus);
     alert("Nebula:" + nebula);
+
+    if (faln > xerus && faln > nebula) {
+      $("#faln").show();
+    }
+
+    if (xerus > faln  && xerus > nebula) {
+      $("#xerus").show();
+    }
+
+    if (nebula > xerus && nebula > faln) {
+      $("#nebula").show();
+    }
 
     event.preventDefault();
   });
